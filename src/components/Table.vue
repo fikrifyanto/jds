4f105d41fb6ed98b48dfb23f08beecabc4253d15
fikrifyanto <fikrifyanto@gmail.com>
2023-09-26 20:52:18 +0700

    feat(pages): add main page
    add new page and add new dependecy; pinia and tailwindcss.

---
+++ src/components/Table.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="mt-12 shadow-sm border rounded-lg overflow-x-auto">
+    <table class="w-full table-auto text-sm text-left">
+      <thead class="bg-gray-50 text-gray-600 font-medium border-b">
+        <tr>
+          <th class="py-3 px-6">Username</th>
+          <th class="py-3 px-6">Email</th>
+          <th class="py-3 px-6">Position</th>
+          <th class="py-3 px-6">Salary</th>
+          <th class="py-3 px-6"></th>
+        </tr>
+      </thead>
+      <tbody class="text-gray-600 divide-y">
+        <template :key="i" v-for="i in 10">
+          <tr>
+            <td class="px-6 py-4 whitespace-nowrap">{item.name}</td>
+            <td class="px-6 py-4 whitespace-nowrap">{item.email}</td>
+            <td class="px-6 py-4 whitespace-nowrap">{item.position}</td>
+            <td class="px-6 py-4 whitespace-nowrap">{item.salary}</td>
+            <td class="text-right px-6 whitespace-nowrap">
+              <a
+                href="javascript:void()"
+                class="py-2 px-3 font-medium text-indigo-600 hover:text-indigo-500 duration-150 hover:bg-gray-50 rounded-lg"
+              >
+                Edit
+              </a>
+              <button
+                href="javascript:void()"
+                class="py-2 leading-none px-3 font-medium text-red-600 hover:text-red-500 duration-150 hover:bg-gray-50 rounded-lg"
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        </template>
+      </tbody>
+    </table>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import Table from '../components/Table.vue'
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <main>
+    <div class="items-start justify-between md:flex">
+      <div class="max-w-lg">
+        <h3 class="text-gray-800 text-xl font-bold sm:text-2xl">Data Penerima Bantuan Sosial</h3>
+        <p class="text-gray-600 mt-2">
+          Data warga penerima bantuan sosial yang terdampak pandemi Covid-19 di Kabupaten Bandung
+          Barat.
+        </p>
+      </div>
+      <div class="mt-3 md:mt-0">
+        <RouterLink
+          to="/create"
+          class="inline-block px-4 py-2 text-white duration-150 font-medium bg-indigo-600 rounded-lg hover:bg-indigo-500 active:bg-indigo-700 md:text-sm"
+        >
+          Tambahkan
+        </RouterLink>
+      </div>
+    </div>
+
+    <Table />
+  </main>
+</template>
